a3edfd043a0117af176bbf534503610263154497
Samu2501 <samuek@gmx.de>
2025-05-21 12:41:37 +0200

    Multiplikation mit double

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main
     public static void main ()
     {
         int obere_grenze = 100;
-        int mult = 1;
+        double mult = 1;
         
         for(int i = 1; i <= obere_grenze; ++i)
         {
