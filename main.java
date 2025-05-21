887f03d45acec59ca0e81565a13e6953760e29f5
SAMUEL KLAR <SAMUEL.KLAR@gmx.de>
2025-05-21 12:47:49 +0200

    Multiplikation mit BigInteger

--- main.java
+++ main.java
@@ -1,14 +1,16 @@
+import java.math.*;
 
 public class main
 {
     public static void main ()
     {
         int obere_grenze = 100;
-        int mult = 1;
+        BigInteger mult = new BigInteger("1");
         
         for(int i = 1; i <= obere_grenze; ++i)
         {
-            mult *= i;
+            String param = String.valueOf(i);
+            mult = mult.multiply(new BigInteger(param));
         }
         
         System.out.println("Mult = " + mult);
