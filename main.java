a87c249eff5f7d94bdd122c60f015877f6e3e804
SAMUEL KLAR <SAMUEL.KLAR@gmx.de>
2025-05-21 12:31:51 +0200

    Multiplikation umgesetzt

--- main.java
+++ main.java
@@ -1,7 +1,16 @@
 
 public class main
 {
-    public static void main (){
-
+    public static void main ()
+    {
+        int obere_grenze = 100;
+        int mult = 1;
+        
+        for(int i = 1; i <= obere_grenze; ++i)
+        {
+            mult *= i;
+        }
+        
+        System.out.println("Mult = " + mult);
     }
 }
